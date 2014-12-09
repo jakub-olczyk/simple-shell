ad3baff1755cbfc457759b7ee9dd4d981145d9b1
Jakub Olczyk <jakub.olczyk@openmailbox.org>
2014-12-09 22:36:32 +0100

    Podstawowa funkcjonalność: znak zachęty, pętla wczytująca polecenia, operacja czyszczenia ekranu, kończenie działania shella

--- main.cpp
+++ main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
-using namespace std;
+#include <string>
+#include <algorithm>
 
-int main(int argc, char** argv){
+const char* ss_version = "v0.1";
+
+void print_prompt(const std::string& currdir)
+{
+	std::cout <<currdir<<"$";
+}
+
+void execute(const std::string& cmd, bool& e)
+{
+	if (cmd == "clear") std::cout << "\x1b\x5b\x48\x1b\x5b\x32\x4a";
+	if (cmd == "exit"){ std::cout << "Bye!"<<std::endl; e=false;}
+}
+
+void with_flags(int argc,char* argv[])
+{
+for (int i=1;i<argc;++i) {
+	if (argv[i][0] != '-') break;
+
+	switch(argv[i][1]){
+		case 'h':
+			std::cout <<"Simple Shell "<< ss_version <<"\ncopyleft Jakub Olczyk\nwritten as an OS learning aid" << std::endl;
+			break;
+		case 'V':
+			std::cout <<"Simple Shell "<< ss_version << std::endl;
+			break;
+	}
+}
+}
+
+int main(int argc, char* argv[])
+{
+if (argc != 1) 
+	with_flags(argc, argv);	
+
+std::string command;	
+std::string pwd = "";	
+
+for(bool end=true;end;){
+	print_prompt(pwd);
+	std::getline(std::cin,command); 
+	execute(command,end);
+}
 
 return 0;
 }
